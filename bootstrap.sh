d77d809c9b683cd722d5377589937040517b1cbe
Sumeru Chatterjee <nodemaker@gmail.com>
2024-05-31 09:24:11 +0200

    Revert bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE[0]}")"
+cd "$(dirname "${BASH_SOURCE}")";
 
-git pull origin master
+git pull origin master;
 
 function doIt() {
-    rsync --exclude ".git/" \
-        --exclude ".DS_Store" \
-        --exclude ".osx" \
-        --exclude "bootstrap.sh" \
-        --exclude "README.md" \
-        --exclude "LICENSE-MIT.txt" \
-        -avh --no-perms . ~
-    source ~/.bash_profile
+        rsync --exclude ".git/" \
+                --exclude ".DS_Store" \
+                --exclude ".osx" \
+                --exclude "bootstrap.sh" \
+                --exclude "README.md" \
+                --exclude "LICENSE-MIT.txt" \
+                -avh --no-perms . ~;
+        source ~/.bash_profile;
 }
 
-if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
-    doIt
-elif [ "$1" == "--no-confirm" ]; then
-    doIt
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+        doIt;
 else
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-    echo ""
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        doIt
-    fi
-fi
-unset doIt
+        read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+        echo "";
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+                doIt;
+        fi;
+fi;
+unset doIt;
 
-# Add current directory to path
+#Add current directory to path
 export PATH=$PWD:$PATH
+
+lets add an option called --no-confirm where it doesn't ask for confirmation
