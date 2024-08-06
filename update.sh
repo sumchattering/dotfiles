44107f7ceb87850c4d78dfaad929506c42ef0a7a
Sumeru Chatterjee <sumeru.chatterjee@xelion.com>
2024-08-06 11:54:18 +0200

    Some updates

--- update.sh
+++ update.sh
@@ -3,14 +3,21 @@
 cd "$(dirname "${BASH_SOURCE}")"
 echo "Current directory: $(pwd)"
 echo "Home directory: $HOME"
+GIT_COMMITTER_NAME=$(git config --get user.name)
+GIT_COMMITTER_EMAIL=$(git config --get user.email)
 function doIt() {
     files_to_update=$(git ls-files)
 
     for file in $files_to_update; do
         home_dir_file="$HOME/$file"
         if [ -f $home_dir_file ]; then
-            cp ~/"$file" "$file"
-            echo "Updated: $file"
+            if [ "$file" == ".gitconfig" ]; then
+                sed '/\[user\]/d; /\s*name = $GIT_COMMITTER_NAME/d; /\s*email = $GIT_COMMITTER_EMAIL/d' ~/"$file" >"$file"
+                echo "Updated and cleaned: $file"
+            else
+                cp ~/"$file" "$file"
+                echo "Updated: $file"
+            fi
         else
             echo "Skipped: $file (not found in home directory)"
         fi
